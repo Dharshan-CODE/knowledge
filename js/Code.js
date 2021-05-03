9017ff2a4951374dc101940272106b0c4260034c
Dharshan-CODE <73929847+Dharshan-CODE@users.noreply.github.com>
2021-05-03 19:26:16 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,42 @@
+const accessCode1 = "JAVASCRIPT";
+const accessCode2 = "JSON";
+const accessCode3 = "GITHUB";
+const accessCode4 = "FIREBASE";
+const accessCode5 = "KNOWLEDGE";
+
+
+
+function clues() {
+    
+    fill("yellow")
+    textSize(15)
+    text(" J V A S R I A T P C", 100,60)
+    fill("white")
+    text("Hint: Programming Language for the Web !!", 100,80)
+// add code for displaying rest of the names and the hints.
+
+    fill("yellow")
+    textSize(15)
+    text("S N J O", 700,100)
+    fill("white")
+    text("Hint: JavaScript object notation syntax!!", 700,120)
+
+    fill("yellow")
+    textSize(15)
+    text("H B U T G I", 100,220)
+    fill("white")
+    text("Hint: A provider of Internet hosting for software development and version control using Git!!", 100,240)
+
+    fill("yellow")
+    textSize(15)
+    text("E A S B I F R E", 450,340)
+    fill("white")
+    text("Hint: A platform developed by Google for creating mobile and web applications!!", 450,360)
+
+    
+    textSize(15)
+    fill("white")
+    text("Question: What will you find inside cave!!", 100,420)
+    
+
+}
